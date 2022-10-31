751226e3df06cf20859a3b66c80976cccbee3b5d
David Pinhas <45331381+davidpinhas@users.noreply.github.com>
2022-10-31 23:59:59 +0200

    remove commands output

--- minecraft-server-install.sh
+++ minecraft-server-install.sh
@@ -6,19 +6,18 @@
 
 # Install prerequisites
 echo "INFO: Installing prerequisites"
-sudo add-apt-repository ppa:openjdk-r/ppa -y
-sudo apt update
-sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y
+sudo add-apt-repository ppa:openjdk-r/ppa -y  > /dev/null 2>&1
+sudo apt update  > /dev/null 2>&1
+sudo apt install apt-transport-https curl gnupg-agent ca-certificates software-properties-common -y  > /dev/null 2>&1
 
 # Install Docker
 if [ -x "$(command -v docker)" ]; then
-    echo "INFO: Docker client already installed"
-    # command
+    echo "INFO: Docker client already installed. Proceeding"
 else
     echo "INFO: Installing Docker client"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-	sudo apt install docker-ce docker-ce-cli containerd.io -y
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y > /dev/null 2>&1
+	sudo apt install docker-ce docker-ce-cli containerd.io -y  > /dev/null 2>&1
 	sudo usermod -aG docker $USER
 	newgrp docker
 fi
@@ -41,12 +40,12 @@ echo "INFO: Checking Docker container status"
 if [ ! "$(docker ps -q -f name=mc-server)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=mc-server)" ]; then
         # cleanup
-        sudo docker rm mc-server
+        sudo docker rm mc-server  > /dev/null 2>&1
         echo "INFO: Cleaned exited container"
     fi
     # run your container
     echo "INFO: Deploying Minecraft server Docker container"
-    sudo docker run -d -it -p 25565:25565 --name mc-server  -e EULA=TRUE --restart unless-stopped -v /home/$USER/minecraft-data:/data itzg/minecraft-server
+    sudo docker run -d -it -p 25565:25565 --name mc-server  -e EULA=TRUE --restart unless-stopped -v /home/$USER/minecraft-data:/data itzg/minecraft-server > /dev/null 2>&1
 fi
 # Setting up server backup job
 sudo cp mc-backup.sh /usr/bin/mc-backup.sh
