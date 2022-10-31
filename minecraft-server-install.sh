3ae3b0727041c997d6e566790cdc77c6e01c5ce9
David Pinhas <45331381+davidpinhas@users.noreply.github.com>
2022-10-31 23:50:50 +0200

    Update install script

--- minecraft-server-install.sh
+++ minecraft-server-install.sh
@@ -33,7 +33,7 @@ then
 else
     echo "WARN: Directory '/home/$USER/minecraft-data' does not exists."
     echo "INFO: Creating directory"
-    mkdir /home/$USER/minecraft-data # Creating Docker volume directory to store MC server configuration files
+    sudo mkdir /home/$USER/minecraft-data # Creating Docker volume directory to store MC server configuration files
 fi
 
 # Running Minecraft Server as a Docker container
@@ -41,13 +41,13 @@ echo "INFO: Checking Docker container status"
 if [ ! "$(docker ps -q -f name=mc-server)" ]; then
     if [ "$(docker ps -aq -f status=exited -f name=mc-server)" ]; then
         # cleanup
-        docker rm mc-server
+        sudo docker rm mc-server
         echo "INFO: Cleaned exited container"
     fi
     # run your container
     echo "INFO: Deploying Minecraft server Docker container"
-    docker run -d -it -p 25565:25565 --name mc-server --restart unless-stopped -v /home/$USER/minecraft-data:/data itzg/minecraft-server
+    sudo docker run -d -it -p 25565:25565 --name mc-server --restart unless-stopped -v /home/$USER/minecraft-data:/data itzg/minecraft-server
 fi
 # Setting up server backup job
-cp mc-backup.sh /usr/local/bin/mc-backup.sh
+cp mc-backup.sh /usr/bin/mc-backup.sh
 echo "INFO: Minecraft server installation script finished"
